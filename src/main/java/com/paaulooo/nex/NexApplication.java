ae615d36c2ddd32577e00e8cb45e35a741432b0b
Paulo <paulorobertofer98@gmail.com>
2025-08-20 10:45:45 -0300

    Spring

---
+++ src/main/java/com/paaulooo/nex/NexApplication.java
@@ -0,0 +1,13 @@
+package com.paaulooo.nex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(NexApplication.class, args);
+	}
+
+}
